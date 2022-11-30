4f65b3745d164bee3d37b1ba0e75a8d9aa67fbf7
salsabilaathika-97 <salsathikar@gmail.com>
2022-11-30 09:59:50 +0800

    add create button in home page

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,11 +1,15 @@
 import react from "react";
 import Navigation from "../../components/Navigation/Navigation";
 import ToDo from "../../components/ToDo/ToDo";
+import Button from 'react-bootstrap/Button';
 
 const Home = () => {
     return (
         <div>
             <Navigation />
+            <div className="mx-8 mt-4 text-center">
+                <Button variant="primary">Create a new to-do list</Button>
+            </div>
             <div className="m-8">
                 <ToDo />
             </div>
