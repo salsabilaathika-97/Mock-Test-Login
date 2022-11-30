be97622ed9c78c1e0f9c1d86f7cc8ed55cf888f1
salsabilaathika-97 <salsathikar@gmail.com>
2022-11-30 08:51:38 +0800

    create router for login and home page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
+import { Routes, Route } from 'react-router';
 import './App.css';
-import Login from './pages/Login';
+import Login from './pages/Login/Login';
+import Home from './pages/Home/Home';
 
 function App() {
   return (
     <div>
-      <Login />
+      <Routes>
+        <Route path = "/" element = {<Login />} />
+        <Route path = "/home" element = {<Home />} />
+      </Routes>
     </div>
   );
 }
